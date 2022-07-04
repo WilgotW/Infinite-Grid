26f75ff5190dfef2f7084490d347ebb931c538cf
Wilgot <wilgotwardell@hotmail.com>
2022-07-04 17:46:06 +0200

    big fixes

--- script.js
+++ script.js
@@ -11,17 +11,26 @@ window.addEventListener('resize', function(){
     setup();
 });
 
-let dots = []
-let lines = []
+let dots = [];
+let lines = [];
+
+const drawDots = false;
+
+let xAndYspace = 50;
+
+// var scale = 1;
+// var originx = 0;
+// var originy = 0;
+
 
 class dot {
     constructor(x, y, radius){
         this.x = x;
         this.y = y;
-        this.radius = radius;
+        this.radius = radius * xAndYspace/40;
     }
     draw(){
-        c.fillStyle = 'blue';
+        c.fillStyle = 'black';
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         c.fill();
@@ -35,9 +44,9 @@ class line {
         this.yLineTo = yLine;
     }
     draw(){
-        c.strokeStyle = 'red';
+        c.strokeStyle = 'black';
         c.beginPath();
-        c.lineWidth = 1;
+        c.lineWidth = 1 * xAndYspace/40; 
         c.moveTo(this.x, this.y);
         c.lineTo(this.xLineTo, this.yLineTo);
         c.stroke();
@@ -45,32 +54,68 @@ class line {
     }
 }
 
+class square {
+    constructor(x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    draw(){
+        c.fillStyle = 'red';
+
+    }
+}
+
 function setup(){
-    for (let i = 0; i < canvas.width/40; i++) {
-        for (let z = 0; z < canvas.height/40; z++){
-            dots.push(new dot(i*40, z*40, 2));
-            console.log('first position:' + i*40 + 'other position' + (i+1)*40);
-            //x:
-            lines.push(new line(i*40, z*40, (i+1)*40, z*40));
-            //y:
-            lines.push(new line(i*40, z*40, i*40, (z+1)*40));
+    for (let i = 0; i < canvas.width/xAndYspace; i++) {
+        for (let z = 0; z < canvas.height/xAndYspace; z++){
+            dots.push(new dot(i*xAndYspace, z*xAndYspace, 2));
+            //x line:
+            lines.push(new line(i*xAndYspace, z*xAndYspace, (i+1)*xAndYspace, z*xAndYspace));
+            //y line:
+            lines.push(new line(i*xAndYspace, z*xAndYspace, i*xAndYspace, (z+1)*xAndYspace));
         }
     }
 }
 setup();
 
 function update(){
+    refrech();
     draw();
     requestAnimationFrame(update);
 }
-
+ 
 function draw(){
-    for(i = 0; i < dots.length; i++){
-        dots[i].draw();
-        
+    // setup();
+    if(drawDots){
+        for(i = 0; i < dots.length; i++){
+            dots[i].draw();
+        }
     }
     for(z = 0; z < lines.length; z++){
         lines[z].draw();
     }
 }
 update();
+
+function refrech(){
+    // for(i = 0; i < lines.length; i++){
+    //     lines.splice(i, 1);
+    // }
+    c.fillStyle = 'white';
+    c.fillRect(0,0, canvas.width, canvas.height);
+}
+// let zoomScale = 0;
+// canvas.onmousewheel = function (event){
+//     zoomScale += event.wheelDelta/120;
+//     xAndYspace += zoomScale;
+//     console.log(xAndYspace);
+//     refrech();
+//     setup();
+// }
+
+window.addEventListener('keydown', function(event){
+    console.log(event.keyCode);
+   
+});
