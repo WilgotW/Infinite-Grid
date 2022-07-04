690dc8abddf8793a1bbcd0ad96895545d9ba74c4
Wilgot <wilgotwardell@hotmail.com>
2022-07-04 16:42:44 +0200

    Added lines between dots

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener('resize', function(){
 });
 
 let dots = []
-
+let lines = []
 
 class dot {
     constructor(x, y, radius){
@@ -27,11 +27,33 @@ class dot {
         c.fill();
     }
 }
+class line {
+    constructor(x, y, xLine, yLine){
+        this.x = x;
+        this.y = y;
+        this.xLineTo = xLine;
+        this.yLineTo = yLine;
+    }
+    draw(){
+        c.strokeStyle = 'red';
+        c.beginPath();
+        c.lineWidth = 1;
+        c.moveTo(this.x, this.y);
+        c.lineTo(this.xLineTo, this.yLineTo);
+        c.stroke();
+
+    }
+}
 
 function setup(){
     for (let i = 0; i < canvas.width/40; i++) {
         for (let z = 0; z < canvas.height/40; z++){
             dots.push(new dot(i*40, z*40, 2));
+            console.log('first position:' + i*40 + 'other position' + (i+1)*40);
+            //x:
+            lines.push(new line(i*40, z*40, (i+1)*40, z*40));
+            //y:
+            lines.push(new line(i*40, z*40, i*40, (z+1)*40));
         }
     }
 }
@@ -45,6 +67,10 @@ function update(){
 function draw(){
     for(i = 0; i < dots.length; i++){
         dots[i].draw();
+        
+    }
+    for(z = 0; z < lines.length; z++){
+        lines[z].draw();
     }
 }
 update();
